7b49f7ebf8816709295b06dd4cb16a255eb432be
Amaroth <Amaroth7@seznam.cz>
2018-02-14 22:21:40 +0100

    Notify user about success.

--- GobGenerator2/Core/CoreController.cs
+++ GobGenerator2/Core/CoreController.cs
@@ -76,7 +76,7 @@ namespace GobGenerator2.Core
         /// </summary>
         public void Generate()
         {
-            //try
+            try
             {
                 sql.SetConnectionInformation(usi.host, usi.port, usi.database, usi.table, usi.login, usi.password);
                 dbc.SetDBCFile(usi.dbcPath);
@@ -87,8 +87,9 @@ namespace GobGenerator2.Core
                 dbc.CreateDisplayIDs(listfile, usi.startDisplayID, usi.useInsert);
                 sql.CreateGameobjects(dbc.GetM2DisplayIDsFromRange(usi.startDisplayID, usi.startDisplayID + listfile.Count - 1, usi.prefix, usi.postfix),
                     dbc.GetWMODisplayIDsFromRange(usi.startDisplayID, usi.startDisplayID + listfile.Count - 1, usi.prefix, usi.postfix), usi.baseEntry, usi.useInsert);
+                MessageBox.Show("Generation process was successful!");
             }
-            //catch (Exception e) { MessageBox.Show("Generation process was not successful. Following error occured.:\n\n" + e.Message); }
+            catch (Exception e) { MessageBox.Show("Generation process was not successful. Following error occured.:\n\n" + e.Message); }
         }
 
         /// <summary>
@@ -108,7 +109,10 @@ namespace GobGenerator2.Core
                 if (usi.exportWMO)
                     dbc.GetWMODisplayIDsFromRange(usi.minDisplayID, usi.maxDisplayID, usi.prefix, usi.postfix);
                 if (m2s.Count + wmos.Count > 0)
+                {
                     sql.CreateGameobjects(m2s, wmos, usi.baseEntry, usi.useInsert);
+                    MessageBox.Show("Generation process was successful!");
+                }
                 else
                     MessageBox.Show("There was nothing found within specified range to import into database.");
             }
